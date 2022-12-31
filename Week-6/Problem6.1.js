0bd5ad7cc1f88faa3d6ba36ee480f633286662ea
Aman Malhotra <aman003malhotra@gmail.com>
2022-12-31 15:38:17 +0530

    week6 tasks

---
+++ Week-6/Problem6.1.js
@@ -0,0 +1,17 @@
+function maxSum(nums){
+    let maxSum = -Infinity;
+    let currSum = 0;
+    for(let i = 0; i< nums.length; i++){
+        currSum += nums[i];
+        if(currSum > maxSum){
+            maxSum  = currSum;
+        }
+        if(currSum < 0){
+            currSum = 0;
+        }
+
+    } 
+    return maxSum;
+}
+
+console.log(maxSum([-2, 1, -3, 4, -1, 2, 1, -5, 4])); // 6
